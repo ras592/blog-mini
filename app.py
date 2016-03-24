9b35ac8358267ad6b8165224fae27c259f146aa6
ras592 <rasharrott@icloud.com>
2016-03-24 17:43:28 -0400

    Updated python documentation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!venv/bin/python
-import web, os, cgi, json
+import web, os, json
 from jinja2 import Environment, FileSystemLoader
 import pymongo
 from bson.objectid import ObjectId
@@ -29,6 +29,14 @@ def render_template(template_name, **context):
     return jinja_env.get_template(template_name).render(context)
 
 def validate(post_id, post_title, post_body):
+    """Gives back a valid dictionary to be converted into JSON.
+    Parameters:
+        post_id: ObjectID object
+        post_title: String
+        post_body: String
+    Returns:
+        dict: Returns the string value of ObjectID, string postTitle, and string postBody.
+    """
     return {
             "postId": str(post_id),
             "postTitle": post_title,
@@ -37,15 +45,22 @@ def validate(post_id, post_title, post_body):
 
 class index:
     def GET(self):
+        """Handles GET method for index route.
+        Renders the template index.html.
+
+        Returns:
+            render_template: Renders index.html template.
+        """
         return render_template('index.html')
 
 class Post:
     def GET(self):
-        """Handles GET method for index route.
-        Query mongodb for posts or handle exceptions and send back an empty list.
+        """Handles GET method for post route.
+        Uses the validate helper method to change the format of the dict to match client
+        json keys.
 
         Returns:
-            render_template: Renders index.html template and list of posts.
+            json: Returns JSON object with the keys: success and array of posts dictionary.
         """
         try:
             posts = db.posts.find()
@@ -62,14 +77,16 @@ class Post:
 
 
     def POST(self):
-        """Handles POST method for index route.
-        Uses cgi.escape() method to validate the input from the user.
+        """Handles POST method for post route.
+        Validates the input from the user.
         Inserts the title and body values into MongoDB and gets the ObjectID
         for the entry.
+        Uses the validate helper method to change the format of the dict to match client
+        json keys.
         Handles exception for any MongoDB errors silently.
 
         Returns:
-            json: Returns JSON object with the keys: success and post_id.
+            json: Returns JSON object with the keys: success and valid_post dictionary.
         """
         inp = web.input()
         title, body = inp.get('postTitle'), inp.get('postBody')
@@ -90,15 +107,11 @@ class Post:
         valid_post = validate(post_id, title, body)
         return json.dumps({'status': 'success', 'post': valid_post})
 
-# Make a second url just for getting and posting posts
-# That way you get all post data from /post GET
-# Add new post data from /post POST
-# Remove any post data from /postDel POST
 
 class PostDel:
     def POST(self):
         """Handles POST method for postDel route.
-        Uses cgi.escape() method to validate the input from the user.
+        Validates the input from the user.
         post_id value is used to remove a document from MongoDB.
         Handles exception for any MongoDB errors silently.
 
